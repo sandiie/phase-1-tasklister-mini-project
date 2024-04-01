6e50922d98f4511785ea9e5f6858eed06c5a2b01
+ <+>
2024-04-01 23:28:24 +0300

    create index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  <form id="create-task-form" action="#" method="POST">
+  <label for="new-task-description">Task description:</label>
+  <input
+    type="text"
+    id="new-task-description"
+    name="new-task-description"
+    placeholder="description"
+  />
+  <input type="submit" value="Create New Task" />
+</form>
+
 });
